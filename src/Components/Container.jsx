88f6e1ed1bd2d04744c1f9a0f14df50783c82e90
vicky-tyson <vickyvital6@gmail.com>
2024-03-07 12:50:45 +0530

    strike to list items feature added

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -6,24 +6,39 @@ export default function Container(){
 const [todoItem, setTodoItem] = useState('');
 const [task, setTask] = useState([]);
 
+const listContainer = document.getElementsByClassName("list-container")
+
+function saveTasks(){
+    localStorage.setItem("data", task);
+}
+
+function displayTaks(){
+   listContainer.innerHTML = localStorage.getItem("data");
+}
+
     function addData(event){
             setTodoItem(event.target.value);
+            saveTasks();
     }
 
     function submitHandler() {
-        if(todoItem == ''){
+        if(todoItem === ''){
             alert("Enter One ToDo")
         } else{
             const newTodos = [...task, todoItem];
             setTask(newTodos);
             setTodoItem("");
+            saveTasks();
         }
     }
 
     function deleteHandler(indexValue){
-        const newtodos = task.filter((todo, index) => index != indexValue);
+        const newtodos = task.filter((todo, index) => index !== indexValue);
         setTask(newtodos);
+        saveTasks();
     }
+
+displayTaks();
 
     return(
         <>
@@ -32,7 +47,7 @@ const [task, setTask] = useState([]);
                     <input type="text" placeholder="Enter Your To-Do" className="todo-data" id="todo" value={todoItem} onChange={addData}/>
                     <button onClick={submitHandler}>Add</button>
                 </div>
-                <ToDoList todolists={task} handleDelete={deleteHandler}/>
+                <ToDoList todolists={task} handleDelete={deleteHandler} task={saveTasks}/>
            </div>
         </>
     )
--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -1,8 +1,15 @@
-export default function ToDoList({todolists, handleDelete}){
+export default function ToDoList({todolists, task, handleDelete}){
+
+    const check = document.getElementsByClassName("list-through");
+
+function makeStrike(indexNum){
+    check[indexNum].classList.add('active');
+    task();
+}
     return(
         <>
            <ul className="todo-list" id="list">
-               {todolists.map((list, index) => <li key={index}>{list} <button className="del-btn" onClick={() => handleDelete(index)}>Delete</button></li>)}
+               {todolists.map((list, index) => <li key={index} className="list-through" onClick={() => makeStrike(index)}>{list} <button className="del-btn" onClick={() => handleDelete(index)}>Delete</button></li>)}
              </ul>
         </>
     )
